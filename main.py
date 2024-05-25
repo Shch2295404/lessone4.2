d22f4310f87d0e5712e616bfe5b4b81010c320a3
Denis <den2295404@gmail.com>
2024-05-25 19:24:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ class Test(object):
         self._protected = "protected attribute"
         self.__private = "private attribute"
 
+    def get_private(self):
+        return self.__private
+
+    def set_private(self, value):
+        self.__private = value
+
 
 test = Test()
 print(test.public)
+print(test.get_private())
+test.set_private("new value")
+print(test.get_private())
