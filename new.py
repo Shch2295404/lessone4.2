8cbf255eb50d334437b16e1022032e54e97f3841
Denis <den2295404@gmail.com>
2024-05-25 20:31:12 +0300

    Update new.py

--- new.py
+++ new.py
@@ -4,18 +4,18 @@ class Car(object):
         self._protected_model = "Camry"
         self.__private_year = 2022
 
-    def public_metod(self):
+    def public_method(self):
         return print(self.public_make)
 
-    def protected_metod(self):
+    def protected_method(self):
         return print(self._protected_model)
 
-    def private_metod(self):
+    def private_method(self):
         return print(self.__private_year)
 
 
 car = Car("Toyota", "Camry", 2022)
 
-print(car.public_metod())
-print(car.protected_metod())
-print(car.private_metod())
+print(car.public_method())
+print(car.protected_method())
+print(car.private_method())
