f5a14a0a51e6a60ff18895abda5d3ed0171098f6
Denis <den2295404@gmail.com>
2024-05-25 19:59:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 class Test(object):
-    def __init__(self):
-        self.public = "public attribute"
-        self._protected = "protected attribute"
-        self.__private = "private attribute"
+    def public_func(self):
+        print("This is public function")
 
-    def get_private(self):
-        return self.__private
+    def _protected_func(self):
+        print("This is protected function")
 
-    def set_private(self, value):
-        self.__private = value
+    def __private_func(self):
+        print("This is private function")
 
 
-test = Test()
-print(test.public)
-print(test.get_private())
-test.set_private("new value")
-print(test.get_private())
+test=Test()
+test.public_func()
