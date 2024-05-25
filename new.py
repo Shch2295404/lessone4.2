b0a9b8c6dd61c6524060f2f9934faa9e9ac7243e
Denis <den2295404@gmail.com>
2024-05-25 20:43:36 +0300

    Update new.py

--- new.py
+++ new.py
@@ -14,8 +14,28 @@ class Car(object):
         return self.__private_year
 
 
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+
+    def public_method(self):
+        return "Electric " + super().public_method()
+
+    def protected_method(self):
+        return "Electric " + super().protected_method()
+
+    def private_method(self) -> str:
+        return "Electric " + str(super().private_method())
+
+
 car = Car("Toyota", "Camry", 2022)
 
 print(car.public_method())
 print(car.protected_method())
 print(car.private_method())
+
+electric_car = ElectricCar("Toyota", "ACDC", 2023)
+
+print(electric_car.public_method())
+print(electric_car.protected_method())
+print(electric_car.private_method())
