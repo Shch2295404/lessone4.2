fd85d92ba585301a231d236e3031a2e1ca666a14
Denis <den2295404@gmail.com>
2024-05-25 19:15:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ class Test(object):
         self._protected = "protected attribute"
         self.__private = "private attribute"
 
-    def public_method(self):
-        print("public method")
 
-    def _protected_method(self):
-        print("protected method")
-
-    def __private_method(self):
-        print("private method")
+test = Test()
+print(test.public)
