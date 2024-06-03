2a064dd118f20af6f294d19652bc9ab1e922f0e8
Denis <den2295404@gmail.com>
2024-06-03 14:19:54 +0300

    edit README

--- new.py
+++ new.py
@@ -1,3 +1,15 @@
+# C:/Users/Щетинин Д Б/Documents/GitHub/lessone4.2/new.py
+# Этот код на языке Python определяет два класса: Car и ElectricCar.
+# Класс Car имеет три атрибута: public_make, _protected_model и __private_year.
+# Атрибут public_make доступен извне класса, а _protected_model и __private_year являются защищенным
+#  и приватным атрибутами соответственно, то есть доступ к ним возможен только изнутри класса или его подклассов.
+# Класс Car также имеет три метода: public_method(), protected_method() и private_method().
+# Метод public_method() возвращает строку, содержащую марку и модель автомобиля,
+#  а методы protected_method() и private_method() возвращают защищенную и частную модель соответственно.
+# Класс ElectricCar является подклассом Car и добавляет дополнительный атрибут battery_size.
+# Он также переопределяет метод get_details(), чтобы включить размер батареи в подробную информацию об электромобиле.
+# Код создает экземпляр класса Car под названием my_car и печатает результат вызова метода public_method().
+# Затем создается экземпляр класса ElectricCar с именем electric_car и выводится результат вызова метода get_details().
 class Car(object):
     def __init__(self, make, model, year):
         self.public_make = make
